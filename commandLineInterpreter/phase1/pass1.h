40e695c3b190938bf6e3934a3a5b00c44f08a997
b-cancel <bryan.o.cancel@gmail.com>
2018-03-22 03:58:40 -0500

    added code that creates opcode table, lets us search it and find an opcode index so we can then access the value from the opcode name key

--- commandLineInterpreter/phase1/pass1.h
+++ commandLineInterpreter/phase1/pass1.h
@@ -4,8 +4,14 @@ Last Updated: 3/20/18
 
 Description:
 Pass 1 will create:
--symbol table
--intermediate file
+-symbol table (label, address[-1 for null])
+-intermediate file (opcode and operand DO NOT HAVE TO be translated into object code)
+	FOR EACH LINE 
+	-copy of source line
+	-value of location counter
+	-values of mnemonics used (since they had to be looked up)
+	-operand (since you had to get it)
+	-error messages (used code and not the actual messages)
 that will be used in pass 2
 
 Deliverable:
@@ -16,30 +22,44 @@ Deliverable:
 5. @toplevel/3334/phase2
 with only source files in said folder
 
-I Was Told
+I Know We Need
 (1) Constant Symbol Table Size of 500
-(2) Ignore anything above START or END
+(2) Ignore anything above START or END ...TODO...
+(3) Order of Command in Assembly is (Label|Operation|Operand|Comments)
+	{label}instruction {operand{,X}} {comment}
+		-items in curly braces are optional
+(4) a line may just have a comment or a label
+	-a label must begin in column 1 (assuming columns are 1 based)
+	-blank column1 means we have no label
+(5) a period in column 1 indicates the entire line is a comment
+(6) blank lines are ignored
+(?) The only restriction is that there must be at least one space or tab separating each field present. The
+only exception is if indexing is requested -- don't put a space between the comma and the X.
 
 I am Assuming: (should find out these details if possible)
-(1) you dont need the (a) intermediate file (b) listing file (c) object file... with any specific file extension --- I will use .txt for all... for now...
-(2) we dont care for case sensitivity
+(1) Everything in "TODO list (maybe)" below is not a requirement
+(2) (a) intermediate file (b) listing file (c) object file -> Dont Require a Specific File Extension (using .txt)
+(3) we dont care for case sensitivity
 */
 
-//TODO list (details)
-//1. chop up strings by the isspace delimter instead of just and actual space
-//2. get reliable length from getline
-//3. set a reliable limit from getline or make sure it has none (so lines can be as long as possible)
-//4. try to remove limits created by the characters if possible
-//5. to the symbol table add (scope info, type[of what?], length[of what?])
-//6. convert the symbol table to something actually efficient
+//TODO list (must)
+//1. remove limit MAX_CHARS_PER_WORD
+//2. maybe read in the Op Code Table
+
+//TODO list (maybe)
+//1. to the symbol table add (scope info, type[of what?], length[of what?])
+//2. to the intermediate file add (pointers to Opcode Table, and pointer to Symbol Table)
+//3. convert the symbol table to something actually efficient (Ideally we use a dynamic Hash Table)
+//4. get reliable length from getline
 
 #pragma once
 
 //constants that will eventually be used throughout code
-#define MAX_CHARS_PER_LINE 100
 #define MAX_CHARS_PER_WORD 100
 #define MAX_SYMBOLS 500
 #define MAX_SYMBOL_SIZE 7 //6 spots and 1 null terminator
+#define OPCOUNT_COUNT 25
+#define LARGEST_OPCODE_LENGTH 5 //4 spots and null terminator
 
 //sic engine tie in
 #include "sic.h"
@@ -51,13 +71,13 @@ I am Assuming: (should find out these details if possible)
 #include <ctype.h>
 #include <assert.h>
 
-//---prototypes
+//---prototypes for string processing
 char* processFirst(char** l);
 char* subString(char* src, int srcIndex, int strLength);
 void subStringRef(char** source, int srcIndex, int strLength);
 int removeSpacesFront(char** line);
 
-//---protoypes for symbol table functions
+//---protoypes for symbol table
 int addSYMTBL(char* key, int value);
 //NOTE: no removal function needed (for now)
 int setSYMTBL(char* key, int value);
@@ -67,14 +87,28 @@ int containsValueSYMTBL(int value);
 int getValueIndexSYMTBL(int value);
 void printSymbolTable();
 
+//---prototypes for opcode table
+void fillOpCodeTable();
+int containsOpCode(char* operand);
+int getOpCodeIndex(char* operand);
+void printOpCodeTable();
+
 //---Symbol Table Global Vars
-typedef struct keyToValue keyToValue;
-struct keyToValue {
+typedef struct charToInt keyToValue;
+struct charToInt {
 	char* key;
 	int value;
 };
 keyToValue symbolTbl[MAX_SYMBOLS]; //keys must be a continuous stream of characters
 int emptyIndex;
+
+//---OpCode Table Global Vars
+typedef struct charToChar charToChar;
+struct charToChar {
+	char* key; //size 5 as set by constant
+	char* value; //size of 2 because that is how much we always need
+};
+charToChar opCodeTbl[OPCOUNT_COUNT];
 
 void pass1(char* filename)
 {
@@ -97,6 +131,8 @@ void pass1(char* filename)
 		//place INTERMEDIATE in stream, mae sure INTERMEDIATE file opens for writing properly
 		ourIntermediateFile = fopen("./intermediate.txt", "w");
 		if (ourIntermediateFile != NULL) {
+
+			fillOpCodeTable();
 
 			int startFound = 0; // 1 for true; if found then start our regular pass1 process; else consider it a comment (space saving, multiline type)
 			int endFound = 0; // 1 for true; if found the end our regular pass1 process... consider lines a comment (space saving, multiline type); else continue regular pass1 process
@@ -165,42 +201,44 @@ char* processFirst(char** l) //actually return our first word found, by referenc
 		//make sure we have string left to check after getting rid of all spaces
 		if (line[lineID] == '\0') {
 			line = '\0'; //nothing useful is left in the line
-			return;
+			return '\0';
 		}
+		else
+		{
+			//used to create both of our substring
+			int firstCharIndex = lineID;
 
-		//used to create both of our substring
-		int firstCharIndex = lineID; 
+			//var init
+			int firstID = 0;
 
-		//var init
-		int firstID = 0;
+			//add anything that isnt a space to our word
+			while (isspace(line[lineID]) == 0 && line[lineID] != '\0' && firstID < MAX_CHARS_PER_WORD) {
+				lineID++;
+				firstID++;
+			}
 
-		//add anything that isnt a space to our word
-		while (isspace(line[lineID]) == 0 && line[lineID] != '\0' && firstID < MAX_CHARS_PER_WORD) {
-			lineID++;
-			firstID++;
+			//NOTE: inclusive index for FIRST start is firstCharIndex... exclusive index for FIRST end is lineID...
+			//size of FIRST is  (lineID - firstCharIndex)
+			//NOTE: inclusive index for LINE start is lineID... exclusive index for LINE end is NOT RELEVANT (use size)
+			//size of LINE is (MAX_SIZE_CONST - lineID)
+			//BEWARE: lineID IS NOT ALWAYS A SPACE
+
+			//calculate first substring
+			int sizeOfFirst = (lineID - firstCharIndex);
+			first = subString(line, firstCharIndex, sizeOfFirst);
+
+			//calculate line substring
+			int sizeOfLine = (strlen(line) - lineID);
+			subStringRef(&line, lineID, sizeOfLine);
+
+			return first;
 		}
-		
-		//NOTE: inclusive index for FIRST start is firstCharIndex... exclusive index for FIRST end is lineID...
-		//size of FIRST is  (lineID - firstCharIndex)
-		//NOTE: inclusive index for LINE start is lineID... exclusive index for LINE end is NOT RELEVANT (use size)
-		//size of LINE is (MAX_SIZE_CONST - lineID)
-		//BEWARE: lineID IS NOT ALWAYS A SPACE
-		
-		//calculate first substring
-		int sizeOfFirst = (lineID - firstCharIndex);
-		first = subString(line, firstCharIndex, sizeOfFirst);
-		
-		//calculate line substring
-		int sizeOfLine = (MAX_CHARS_PER_LINE - lineID);
-		subStringRef(&line, lineID, sizeOfLine);
-
-		return first;
 	}
 	else
 		return '\0';
 }
 
-//---Helper Functions 
+//-------------------------String Parsing and Tokenizing Functions 
 
 char* subString(char* src, int srcIndex, int strLength) {
 
@@ -235,8 +273,6 @@ void subStringRef(char** source, int srcIndex, int strLength) { //pass src by re
 	src[destI + 1] = '\0';
 }
 
-//---Extra Functions
-
 int removeSpacesFront(char** line) { //returns how many spaces where removed
 
 	char* l = *line;
@@ -253,13 +289,13 @@ int removeSpacesFront(char** line) { //returns how many spaces where removed
 	if (line[lineID] == '\0')
 		line = '\0'; //nothing useful is left in the line
 
-	int sizeOfLine = (MAX_CHARS_PER_LINE - lineID);
+	int sizeOfLine = (strlen(l) - lineID);
 	subStringRef(line, lineID, sizeOfLine);
 
 	return lineID;
 }
 
-//----------Symbol Table Print (Symbol | Location)
+//-------------------------Symbol Table Functions (Symbol | Location)
 
 int addSYMTBL(char* key, int value) { //returns 1 if success, 0 if value must be set, -1 if key to long, -2 if symbolTbl full
 
@@ -329,8 +365,109 @@ int getValueIndexSYMTBL(int value) {
 
 void printSymbolTable() {
 	printf("---Symbol Table (string -> int)\n");
-	for (int i = 0; i < emptyIndex; i++) {
+	for (int i = 0; i < emptyIndex; i++)
 		printf("'%s' maps to '%i'\n", symbolTbl[i].key, symbolTbl[i].value);
+	printf("\n");
+}
+
+//-------------------------Op Code Table Functions (Symbol | Location)
+
+void fillOpCodeTable() {
+
+	int index = 0;
+	for (int index = 0; index < OPCOUNT_COUNT; index++) {
+
+		//allocate memory for both values
+		opCodeTbl[index].key = malloc(LARGEST_OPCODE_LENGTH * sizeof(char)); //allocate memory for char
+		opCodeTbl[index].value = malloc(2 * sizeof(char));
+
+		//allocate memory for temporary values
+		char* theKey = malloc(LARGEST_OPCODE_LENGTH * sizeof(char));;
+		char* theValue = malloc(2 * sizeof(char));
+
+		switch (index)
+		{
+		case 0: //	ADD m 				18 		A <-(A)+(m..m + 2)
+			theKey = "ADD";	theValue = "18";	break;
+		case 1: //	AND m 				58 		A <-(A) & (m..m + 2)[bitwise]
+			theKey = "AND";	theValue = "58";	break;
+		case 2: //	COMP m 				28 		cond code <-(A) : (m..m + 2)
+			theKey = "COMP";	theValue = "28";	break;
+		case 3: //	DIV m 				24 		A <-(A) / (m..m + 2)
+			theKey = "DIV";	theValue = "24";	break;
+		case 4: //	J m 				3C 		PC <-m
+			theKey = "J";	theValue = "3C";	break;
+		case 5: //	JEQ m 				30 		PC <-m if cond code set to =
+			theKey = "JEQ";	theValue = "30";	break;
+		case 6: //	JGT m 				34 		PC <-m if cond code set to >
+			theKey = "JGT";	theValue = "34";	break;
+		case 7: //	JLT m 				38 		PC <-m if cond code set to <
+			theKey = "JLT";	theValue = "38";	break;
+		case 8: //	JSUB m 				48 		L <-(PC); PC <-m
+			theKey = "JSUB";	theValue = "48";	break;
+		case 9: //	LDA m 				00 		A <-(m..m + 2)
+			theKey = "LDA";	theValue = "00";	break;
+		case 10: //	LDCH m 				50 		A[rightmost byte] <-(m)
+			theKey = "LDCH";	theValue = "50";	break;
+		case 11: //	LDL m 				08 		L <-(m..m + 2)
+			theKey = "LDL";	theValue = "08";	break;
+		case 12: //	LDX m 				04 		X <-(m..m + 2)
+			theKey = "LDX";	theValue = "04";	break;
+		case 13: //	MUL m 				20 		A <-(A) * (m..m + 2)
+			theKey = "MUL";	theValue = "20";	break;
+		case 14: //	OR m 				44 		A <-(A) | (m..m + 2)[bitwise]
+			theKey = "OR";	theValue = "44";	break;
+		case 15: //	RD m 				D8 		A[rightmost byte] <-data from device specified by(m)
+			theKey = "RD";	theValue = "D8";	break;
+		case 16: //	RSUB 				4C 		PC <-(L)
+			theKey = "RSUB";	theValue = "4C";	break;
+		case 17: //	STA m 				0C 		m..m + 2 <-(A)
+			theKey = "STA";	theValue = "0C";	break;
+		case 18: //	STCH m 				54 		m <-(A)[rightmost byte]
+			theKey = "STCH";	theValue = "54";	break;
+		case 19: //	STL m 				14 		m..m + 2 <-(L)
+			theKey = "STL";	theValue = "14";	break;
+		case 20: //	STX m 				10 		m..m + 2 <-(X)
+			theKey = "STX";	theValue = "10";	break;
+		case 21: //	SUB m 				1C 		A <-(A)-(m..m + 2)
+			theKey = "SUB";	theValue = "1C";	break;
+		case 22: //	TD m 				E0 		Test device specified by(m)
+			theKey = "TD";	theValue = "E0";	break;
+		case 23: //	TIX m 				2C 		X <-(X)+1; compare X and (m..m + 2)
+			theKey = "TIX";	theValue = "2C";	break;
+		case 24: //	WD m 				DC 		Device specified by(m) <-(A)[rightmost byte]
+			theKey = "WD";	theValue = "DC";	break;
+		default:
+			break;
+		}
+
+		//assign values to dictionary
+		opCodeTbl[index].key = theKey;
+		opCodeTbl[index].value = theValue;
+
+		//un allocate memory for temporary values (this is causing erros in linux)
+		//free(theKey);
+		//free(theValue);
 	}
+}
+
+int containsOpCode(char* operand) {
+	if (getOpCodeIndex(operand) != -1)
+		return 1;
+	else
+		return 0;
+}
+
+int getOpCodeIndex(char* operand) {
+	for (int i = 0; i < OPCOUNT_COUNT; i++)
+		if (strcmp(opCodeTbl[i].key, operand) == 0)
+			return i;
+	return -1;
+}
+
+void printOpCodeTable() {
+	printf("---Op Code Table (string -> int)\n");
+	for (int i = 0; i < OPCOUNT_COUNT; i++)
+		printf("'%s' maps to '%s'\n", opCodeTbl[i].key, opCodeTbl[i].value);
 	printf("\n");
 }
