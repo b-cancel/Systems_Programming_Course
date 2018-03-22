5ada802d0b0243c04402ae7159998b840f7578b6
b-cancel <bryan.o.cancel@gmail.com>
2018-03-22 01:16:07 -0500

    code for symbol table added, add, set, contains key, contains value, find index of key, find index of value, and print entire table functions added... no remove function... symbol table is currently just an array of structs... it really should work as a dictionary but right now everything searches sequentially O(500) because 500 is size of the array... all errors work and primarily they can occur when adding

--- commandLineInterpreter/phase1/pass1.h
+++ commandLineInterpreter/phase1/pass1.h
@@ -16,9 +16,13 @@ Deliverable:
 5. @toplevel/3334/phase2
 with only source files in said folder
 
-I am Assuming:
+I Was Told
+(1) Constant Symbol Table Size of 500
+(2) Ignore anything above START or END
+
+I am Assuming: (should find out these details if possible)
 (1) you dont need the (a) intermediate file (b) listing file (c) object file... with any specific file extension --- I will use .txt for all... for now...
-(2) I can write whatever I want before START and after END and it should not affect file (c), but it will appear as a comment
+(2) we dont care for case sensitivity
 */
 
 //TODO list (details)
@@ -26,13 +30,16 @@ I am Assuming:
 //2. get reliable length from getline
 //3. set a reliable limit from getline or make sure it has none (so lines can be as long as possible)
 //4. try to remove limits created by the characters if possible
-//5. find out if case sensitive matter on directive and mneumonics
+//5. to the symbol table add (scope info, type[of what?], length[of what?])
+//6. convert the symbol table to something actually efficient
 
 #pragma once
 
 //constants that will eventually be used throughout code
 #define MAX_CHARS_PER_LINE 100
 #define MAX_CHARS_PER_WORD 100
+#define MAX_SYMBOLS 500
+#define MAX_SYMBOL_SIZE 7 //6 spots and 1 null terminator
 
 //sic engine tie in
 #include "sic.h"
@@ -44,20 +51,42 @@ I am Assuming:
 #include <ctype.h>
 #include <assert.h>
 
-//prototypes
+//---prototypes
 char* processFirst(char** l);
-char* processRest(char** l);
 char* subString(char* src, int srcIndex, int strLength);
 void subStringRef(char** source, int srcIndex, int strLength);
 int removeSpacesFront(char** line);
+
+//---protoypes for symbol table functions
+int addSYMTBL(char* key, int value);
+//NOTE: no removal function needed (for now)
+int setSYMTBL(char* key, int value);
+int containsKeySYMTBL(char* key);
+int getKeyIndexSYMTBL(char* key);
+int containsValueSYMTBL(int value);
+int getValueIndexSYMTBL(int value);
+void printSymbolTable();
+
+//---Symbol Table Global Vars
+typedef struct keyToValue keyToValue;
+struct keyToValue {
+	char* key;
+	int value;
+};
+keyToValue symbolTbl[MAX_SYMBOLS]; //keys must be a continuous stream of characters
+int emptyIndex;
 
 void pass1(char* filename)
 {
 	printf("running pass 1\n");
 
+	//---testing area start
+
+	//---testing area end
+
+	//---Stream Inits
 	FILE *ourSourceFile;
 	FILE *ourIntermediateFile;
-	FILE *ourSymbolTableFile;
 
 	//NOTE: all errors should be passed in a single run (errors should not be printed but instead written to the intermediate file)
 
@@ -87,7 +116,7 @@ void pass1(char* filename)
 				printf("after removing string '%s' we have line '%s'\n", p1, line);
 
 				removeSpacesFront(&line);
-				printf("line no longer with space in the front '%s'\,", line);
+				printf("line no longer with space in the front '%s'\n", line);
 
 				//---test code
 				
@@ -115,8 +144,6 @@ void pass1(char* filename)
 }
 
 //-------------------------EXTRA PROCS-------------------------
-
-//----------Handle Reading
 
 //----------Handle Tockenizing
 
@@ -171,10 +198,6 @@ char* processFirst(char** l) //actually return our first word found, by referenc
 	}
 	else
 		return '\0';
-}
-
-char* processRest(char** l) {
-	return 'i';
 }
 
 //---Helper Functions 
@@ -237,3 +260,77 @@ int removeSpacesFront(char** line) { //returns how many spaces where removed
 }
 
 //----------Symbol Table Print (Symbol | Location)
+
+int addSYMTBL(char* key, int value) { //returns 1 if success, 0 if value must be set, -1 if key to long, -2 if symbolTbl full
+
+	if (emptyIndex < MAX_SYMBOLS) {
+		if (strlen(key) < MAX_SYMBOL_SIZE) {
+			int newValueInsert = emptyIndex; //location we insert the key and value into their respective arrays
+			if (getKeyIndexSYMTBL(key) == -1) { //they key is not in our arrays
+												//new value inserted
+				symbolTbl[newValueInsert].key = malloc(MAX_SYMBOL_SIZE * sizeof(char)); //allocate memory for char
+				symbolTbl[newValueInsert].key = key; //save char
+				symbolTbl[newValueInsert].value = value;
+				emptyIndex++;
+				return 1;
+			}
+			else
+				return 0;
+		}
+		else
+			return -1;
+	}
+	else
+		return -2;
+}
+
+int setSYMTBL(char* key, int value) { //returns 1 if key and value pairing found and update, 0 otherwise
+
+	
+	int oldValueOverwrite = getKeyIndexSYMTBL(key); //get the index of key...
+	if (oldValueOverwrite != -1) { //they key is not in our arrays
+		//old value overwritten
+		symbolTbl[oldValueOverwrite].value = value;
+		return 1;
+	}
+	else
+		return 0;
+}
+
+//NOTE: no removal function needed (for now)
+
+int containsKeySYMTBL(char* key) {
+	if (getKeyIndexSYMTBL(key) != -1) //it exists
+		return 1;
+	else
+		return 0;
+}
+
+int getKeyIndexSYMTBL(char* key) {
+	for (int i = 0; i < emptyIndex; i++)
+		if (strcmp(symbolTbl[i].key, key) == 0)
+			return i;
+	return -1;
+}
+
+int containsValueSYMTBL(int value) {
+	if (getValueIndexSYMTBL(value) != -1) //it exists
+		return 1;
+	else
+		return 0;
+}
+
+int getValueIndexSYMTBL(int value) {
+	for (int i = 0; i < emptyIndex; i++)
+		if ((symbolTbl[i].value) == value)
+			return i;
+	return -1;
+}
+
+void printSymbolTable() {
+	printf("---Symbol Table (string -> int)\n");
+	for (int i = 0; i < emptyIndex; i++) {
+		printf("'%s' maps to '%i'\n", symbolTbl[i].key, symbolTbl[i].value);
+	}
+	printf("\n");
+}
